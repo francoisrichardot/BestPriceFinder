fe42fdde81c35214dda45668758578d2d6d16bee
Francois Richardot <francoisrichardot@gmail.com>
2019-04-15 19:38:36 -0400

    Initial commit with Bottom navigation menu and fragments

---
+++ app/src/main/java/com/example/frank/bestpricefinder/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.frank.bestpricefinder;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+            Fragment fragment = null;
+
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+
+                case R.id.navigation_lists:
+                    fragment = new ListsFragment();
+                    break;
+                case R.id.navigation_scan:
+                    fragment = new ScanFragment();
+                    break;
+                case R.id.navigation_specials:
+                    fragment = new SpecialsFragment();
+                    break;
+
+                case R.id.navigation_settings:
+                    fragment = new SettingsFragment();
+                    break;
+            }
+
+            return loadFragment(fragment);
+            // return false;
+        }
+    };
+
+
+    /*_____________________________________loadFragment_________________________________________*/
+   /* Method to load fragments in the MainActivity fragments container.*/
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    /*__________________________________End of loadFragment___________________________________*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
