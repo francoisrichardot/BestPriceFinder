0f22cedd3e44fe15925d03351b4a2a86b21e273d
Francois Richardot <francoisrichardot@gmail.com>
2019-04-16 22:11:29 -0400

    Ajouter fonctionalité de FireBase, Ajouter ML Kit Barcode Scanning API pour le scan de barcode(folder: barcodescanning) Ajouter fonctionalité de caméra(folder: camerautility) Integré le barcode scan dans 'ScanFrangment'

--- app/src/main/java/com/example/frank/bestpricefinder/ScanFragment.java
+++ app/src/main/java/com/example/frank/bestpricefinder/ScanFragment.java
@@ -8,14 +8,188 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class ScanFragment extends Fragment {
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+
+import com.example.frank.bestpricefinder.barcodescanning.BarcodeScanningProcessor;
+import com.example.frank.bestpricefinder.camerautility.CameraSource;
+import com.example.frank.bestpricefinder.camerautility.CameraSourcePreview;
+import com.example.frank.bestpricefinder.camerautility.GraphicOverlay;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScanFragment extends Fragment implements
+        CompoundButton.OnCheckedChangeListener {
+
+
+    private static final int PERMISSION_REQUESTS = 1;
+    private CameraSource cameraSource = null;
+    private CameraSourcePreview preview;
+    private GraphicOverlay graphicOverlay;
+    private static final String TAG = "Barcode Scanning";
+
     @Nullable
-    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //just change the fragment_dashboard
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_scan, null);
+        View view = inflater.inflate(R.layout.fragment_scan, container, false);
+        //return inflater.inflate(R.layout.fragment_scan, null);
+
+        //setContentView(R.layout.activity_main);
+        preview = view.findViewById(R.id.firePreview);
+        graphicOverlay = view.findViewById(R.id.fireFaceOverlay);
+
+
+        if (allPermissionsGranted()) {
+            createCameraSource();
+            startCameraSource();
+        } else {
+            getRuntimePermissions();
+        }
+
+
+
+        return view;
     }
+
+
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+
+        if (cameraSource != null) {
+            if (isChecked) {
+                cameraSource.setFacing(CameraSource.CAMERA_FACING_FRONT);
+            } else {
+                cameraSource.setFacing(CameraSource.CAMERA_FACING_BACK);
+            }
+        }
+        preview.stop();
+        startCameraSource();
+    }
+
+
+    private void createCameraSource() {
+        // If there's no existing cameraSource, create one.
+        if (cameraSource == null) {
+            cameraSource = new CameraSource(getActivity(), graphicOverlay);
+        }
+        cameraSource.setMachineLearningFrameProcessor(new BarcodeScanningProcessor());
+
+    }
+
+
+    /**
+     * Starts or restarts the camera source, if it exists. If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    private void startCameraSource() {
+        if (cameraSource != null) {
+            try {
+                if (preview == null) {
+                    Log.d(TAG, "resume: Preview is null");
+                }
+                if (graphicOverlay == null) {
+                    Log.d(TAG, "resume: graphOverlay is null");
+                }
+                preview.start(cameraSource, graphicOverlay);
+            } catch (IOException e) {
+                cameraSource.release();
+                cameraSource = null;
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startCameraSource();
+    }
+
+    /** Stops the camera. */
+    @Override
+    public void onPause() {
+        super.onPause();
+        preview.stop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (cameraSource != null) {
+            cameraSource.release();
+        }
+    }
+    private boolean allPermissionsGranted() {
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(getActivity(), permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private void getRuntimePermissions() {
+        List allNeededPermissions = new ArrayList<>();
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(getActivity(), permission)) {
+                allNeededPermissions.add(permission);
+            }
+        }
+
+        if (!allNeededPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    getActivity(), (String[]) allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+        }
+    }
+    private String[] getRequiredPermissions() {
+        try {
+            PackageInfo info =
+                    getActivity().getPackageManager()
+                            .getPackageInfo(getActivity().getPackageName(), PackageManager.GET_PERMISSIONS);
+            String[] ps = info.requestedPermissions;
+            if (ps != null && ps.length > 0) {
+                return ps;
+            } else {
+                return new String[0];
+            }
+        } catch (Exception e) {
+            return new String[0];
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        if (allPermissionsGranted()) {
+            createCameraSource();
+            startCameraSource();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private static boolean isPermissionGranted(Context context, String permission) {
+        if (ContextCompat.checkSelfPermission(context, permission)
+                == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
 }
